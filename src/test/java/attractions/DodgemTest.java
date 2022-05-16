3419e4cfd313a50ed7a123a2abacf4c1c54b897d
Graeme-Dunlop <graemedunlop1983@gmail.com>
2022-05-16 16:06:36 +0100

    MVP done

---
+++ src/test/java/attractions/DodgemTest.java
@@ -0,0 +1,40 @@
+package attractions;
+
+import org.junit.Before;
+import org.junit.Test;
+import people.Visitor;
+
+import static org.junit.Assert.assertEquals;
+
+public class DodgemTest {
+
+    Dodgems dodgems;
+    Visitor visitor;
+
+    @Before
+    public void setUp() throws Exception {
+        dodgems = new Dodgems("Bumper Cars", 5);
+        visitor = new Visitor(11, 2.1, 40.0);
+    }
+
+
+    @Test
+    public void hasName() {
+        assertEquals("Bumper Cars", dodgems.getName());
+    }
+
+    @Test
+    public void hasRating() {
+        assertEquals(5, dodgems.getRating());
+    }
+
+    @Test
+    public void hasVisitCount() {
+        assertEquals(0, dodgems.getVisitCount());
+    }
+
+    @Test
+    public void canChargeHalfPrice(){
+        assertEquals(2.25, dodgems.priceFor(visitor), 0.0);
+    }
+}
---
+++ src/test/java/attractions/RollercoasterTest.java
@@ -0,0 +1,44 @@
+package attractions;
+
+import org.junit.Before;
+import org.junit.Test;
+import people.Visitor;
+
+import static org.junit.Assert.assertEquals;
+
+public class RollercoasterTest {
+
+    RollerCoaster rollerCoaster;
+    Visitor visitor;
+
+    @Before
+    public void setUp() {
+        rollerCoaster = new RollerCoaster("Blue Ridge", 10);
+        visitor = new Visitor(14, 2.1, 40.0);
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("Blue Ridge", rollerCoaster.getName());
+    }
+
+    @Test
+    public void hasRating() {
+        assertEquals(10, rollerCoaster.getRating());
+    }
+
+    @Test
+    public void hasVisitCount() {
+        assertEquals(0, rollerCoaster.getVisitCount());
+    }
+
+    @Test
+    public void isNotAllowedToRide(){
+        assertEquals(false, rollerCoaster.isAllowedTo(visitor));
+    }
+
+    @Test
+    public void canChargeDouble(){
+        assertEquals(16.80, rollerCoaster.priceFor(visitor), 0.0);
+    }
+}
